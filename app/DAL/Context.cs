bd61ce058bffe0035a6ed010f94c98de077427c2
Marvin Antonio Mejia Espinoza <srgmejia@icloud.com>
2025-03-22 11:44:32 -0600

    Agregando contexto

---
+++ app/DAL/Context.cs
@@ -0,0 +1,11 @@
+using EL;
+using Microsoft.EntityFrameworkCore;
+
+public class Context:DbContext
+{
+    public virtual DbSet<Cliente> Cliente {get;set;}
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Server=alldevsolutions.net,1460\DEV2022;Database=Facturacion;User Id=univalle;Password=#Univalle#123;TrustServerCertificate=True;");
+    }
+}
