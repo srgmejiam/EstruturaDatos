3be95e2d126c61af6d3cb047359d17d6032516aa
Marvin Antonio Mejia Espinoza <srgmejia@icloud.com>
2025-04-10 16:43:17 -0600

    ajustando cadena de conexion

--- app/DAL/Context.cs
+++ app/DAL/Context.cs
@@ -6,6 +6,6 @@ public class Context:DbContext
     public virtual DbSet<Cliente> Cliente {get;set;}
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.UseSqlServer(@"Server=69.197.174.111,1460\DEV2022;Database=Facturacion;User Id=univalle;Password=#Univalle#123;TrustServerCertificate=True;");
+        optionsBuilder.UseSqlServer(@"Server=services.alldevsolutions.net,1460\DEV2022;Database=Facturacion;User Id=univalle;Password=#Univalle#123;TrustServerCertificate=True;");
     }
 }
